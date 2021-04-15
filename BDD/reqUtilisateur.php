912bf76ca7487dc7c745c2f03d332710b24d3468
Onur Tan Kulan <46331545+Tnkln99@users.noreply.github.com>
2021-04-15 11:33:08 +0200

    Update reqUtilisateur.php

--- BDD/reqUtilisateur.php
+++ BDD/reqUtilisateur.php
@@ -18,14 +18,12 @@
 		$row = $res->fetch_assoc();
 		
 		$connexion->close();
-		
 		return $res->num_rows;
 	}
 	
 	function insertUser(string $nom, string $prenom, string $email, string $mdp, string $confirmation, string $role)
 	{
 		include('DataBaseLogin.inc.php');
-		
 		$connexion = new mysqli($server, $user, $passwd, $db);
 	
 		if($connexion->connect_error)
@@ -34,28 +32,36 @@
 		}
 		
 		$idU = lineCount("Utilisateur") + 1;
-		
 		if(strcmp($mdp, $confirmation) != 0)
 			trigger_error("Le mot de passe et la confirmation ne correspondent pas.");
 		
 		if(!filter_var($email, FILTER_VALIDATE_EMAIL))
 			trigger_error("$email n'est pas une adresse mail.");
 		
-		if((strcmp($role, "Utilisateur") != 0) && (strcmp($role, "Administrateur") != 0))
+		if((strcmp($role, "Utilisateur") != 0) && strcmp($role, "Joueur") != 0)
 			trigger_error("Le rôle de l'utilisateur est invalide.");
-		
-		$requete = "INSERT INTO Utilisateur VALUES($idU, '$nom', '$prenom', '$email', '$mdp', '$role');";
+		$requete = "INSERT INTO Utilisateur VALUES($idU, '$nom', '$prenom', '$email', '$mdp', 'Utilisateur');";
 		
 		$res = $connexion->query($requete);
 		if(!$res)
 			die('Echec lors de l\'exécution de la requête: ('.$connexion->errno.') '.$connexion->error);
-		
+
+		$connexion->close();	
+	}
+
+	function insertJouer(bool $b){
+		include('DataBaseLogin.inc.php');
+		$connexion = new mysqli($server, $user, $passwd, $db);
+		if($connexion->connect_error)
+		{
+			echo('Erreur de connexion('.$connexion->connect_errno.') '.$connexion->connect_error);
+		}
+		$idU = lineCount("Utilisateur");
+		$requete_InsJ = "INSERT INTO Joueur (idJoueur,idEquipe,estCapitaine) VALUES ($idU,0 ,$b) ";
+		$resJ = $connexion->query($requete_InsJ);
+		if(!$resJ)
+			die('Echec lors de l\'exécution de la requête requete_InsU: ('.$connexion->errno.') '.$connexion->error);
 		$connexion->close();
-		
-		unset($_POST);
-		
-		header('Location: ../php/Login.php');
-		exit();
 	}
 	
 	function verifLogin(string $login)
@@ -80,7 +86,7 @@
 			return false;
 		}
 		
-		$res->data_seek(0);
+		$res->data_seek(0); //bunu sormayı unutma !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 		$verif = $res->fetch_assoc()["idUtilisateur"];
 		
 		$connexion->close();
@@ -125,152 +131,5 @@
 			return false;
 		
 		return true;
-	}
-	
-	function estJoueur(string $id)
-	{
-		include('DataBaseLogin.inc.php');
-		
-		$connexion = new mysqli($server, $user, $passwd, $db);
-	
-		if($connexion->connect_error)
-		{
-			echo('Erreur de connexion('.$connexion->connect_errno.') '.$connexion->connect_error);
-		}
-		
-		$requete = "SELECT idJoueur FROM Joueur WHERE idJoueur = \"$id\";";
-		
-		$res = $connexion->query($requete);
-		if(!$res)
-		{
-			die('Echec lors de l\'exécution de la requête: ('.$connexion->errno.') '.$connexion->error);
-			$connexion->close();
-			
-			return false;
-		}
-		
-		$res->data_seek(0);
-		$verif = $res->fetch_assoc()["idJoueur"];
-		
-		$connexion->close();
-		
-		if(empty($verif))
-			return false;
-		
-		return true;
-	}
-	
-	function getUtilisateur(string $id)
-	{
-		include('DataBaseLogin.inc.php');
-		include('../module/Utilisateur.php');
-		
-		$connexion = new mysqli($server, $user, $passwd, $db);
-	
-		if($connexion->connect_error)
-		{
-			echo('Erreur de connexion('.$connexion->connect_errno.') '.$connexion->connect_error);
-		}
-		
-		$requete = "SELECT * FROM Utilisateur WHERE idUtilisateur = \"$id\";";
-		
-		$res = $connexion->query($requete);
-		if(!$res)
-		{
-			die('Echec lors de l\'exécution de la requête: ('.$connexion->errno.') '.$connexion->error);
-			$connexion->close();
-			
-			return NULL;
-		}
-		
-		$res->data_seek(0);
-		$idUtilisateur = $res->fetch_assoc()["idUtilisateur"];
-		$nom = $res->fetch_assoc()["nom"];
-		$prenom = $res->fetch_assoc()["prenom"];
-		$email = $res->fetch_assoc()["email"];
-		$motDePasse = $res->fetch_assoc()["motDePasse"];
-		$role = $res->fetch_assoc()["role"];
-		
-		$connexion->close();
-		
-		if(empty($idUtilisateur))
-			return NULL;
-		
-		return new Utilisateur($idUtilisateur, $nom, $prenom, $email, $motDePasse, $role);
-	}
-	
-	function getUtilisateurWithEmail(string $login)
-	{
-		include('DataBaseLogin.inc.php');
-		include('../module/Utilisateur.php');
-		
-		$connexion = new mysqli($server, $user, $passwd, $db);
-	
-		if($connexion->connect_error)
-		{
-			echo('Erreur de connexion('.$connexion->connect_errno.') '.$connexion->connect_error);
-		}
-		
-		$requete = "SELECT * FROM Utilisateur WHERE email = \"$login\";";
-		
-		$res = $connexion->query($requete);
-		if(!$res)
-		{
-			die('Echec lors de l\'exécution de la requête: ('.$connexion->errno.') '.$connexion->error);
-			$connexion->close();
-			
-			return NULL;
-		}
-		
-		$res->data_seek(0);
-		$idUtilisateur = $res->fetch_assoc()["idUtilisateur"];
-		$nom = $res->fetch_assoc()["nom"];
-		$prenom = $res->fetch_assoc()["prenom"];
-		$email = $res->fetch_assoc()["email"];
-		$motDePasse = $res->fetch_assoc()["motDePasse"];
-		$role = $res->fetch_assoc()["role"];
-		
-		$connexion->close();
-		
-		if(empty($idUtilisateur))
-			return NULL;
-		
-		return new Utilisateur($idUtilisateur, $nom, $prenom, $email, $motDePasse, $role);
-	}
-	
-	function getJoueur(string $id)
-	{
-		include('DataBaseLogin.inc.php');
-		
-		$connexion = new mysqli($server, $user, $passwd, $db);
-	
-		if($connexion->connect_error)
-		{
-			echo('Erreur de connexion('.$connexion->connect_errno.') '.$connexion->connect_error);
-		}
-		
-		$requete = "SELECT * FROM Joueur WHERE idJoueur = \"$id\";";
-		
-		$res = $connexion->query($requete);
-		if(!$res)
-		{
-			die('Echec lors de l\'exécution de la requête: ('.$connexion->errno.') '.$connexion->error);
-			$connexion->close();
-			
-			return NULL;
-		}
-		
-		$res->data_seek(0);
-		$idJoueur = $res->fetch_assoc()["idJoueur"];
-		$estCapitaine = $res->fetch_assoc()["estCapitaine"];
-		
-		$connexion->close();
-		
-		if(empty($idJoueur))
-			return NULL;
-		
-		$ut = getUtilisateur($id);
-		
-		return new Joueur($ut->getIdJoueur(), $ut->getNom(), $ut->getPrenom(), $ut->getEmail(), $ut->getMdp(), $ut->getRole(), $idJoueur, $estCapitaine);
 	}
 ?>
