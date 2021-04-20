d33567a508226bf61d5017d0ba0060a0f9a6c95c
Onur Tan Kulan <46331545+Tnkln99@users.noreply.github.com>
2021-04-20 23:03:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
             <a href="#">Tournois en cours</a>
             <a href="Tournois.php">Liste des Tournois</a>
             <a href="php/Preinscription.php">Preinscription</a>
+            <a href="php/AffectGest.php">affecter les gestionnaire</a>
             <div class="topnav-right">
               <a href="php/Login.php">Login</a>
-			  <a href="php/Logout.php">Se déconnecter</a>
             </div>
-        </div>
+        </div>          
     </body>
 </html>
