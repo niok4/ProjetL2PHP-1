7ad845c67a77b97b1bc83bd1f1570a0d98278b65
BlackWitch1 <71940835+BlackWitch1@users.noreply.github.com>
2021-04-26 13:08:18 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,15 +3,36 @@
 <head>
     <link rel="stylesheet" type="text/css" href="./css/syleIndex.css" />
     <!--<script type="text/javascript" src="PageDacc.js"></script>-->
-
+    <title>Accueil</title>
+</head>
     <body>
         <div class="topnav">
-            <a href="#">Information</a>
-            <a href="#">Tournois en cours</a>
-            <a href="Tournois.php">Liste des Tournois</a>
-            <div class="topnav-right">
-              <a href="php/Login.php">Login</a>
+            <div class="carre">
+                <img src="/img/logo.jpeg">
             </div>
-        </div>          
+            <a href="#">Informations</a>
+            <a href="php/AffectGest.php">Gestionnaire</a>
+            <a href="php/creer_tournois.php">Créer Tournoi</a>
+            <a href="php/tournois.php">Liste des Tournois</a>
+            <a href="php/Preinscription.php">Preinscription</a>
+              <a href="php/CreerEquipe.php">Créer équipe</a>
+            <a href="php/Tests.php">testArbre</a>
+            <div class="topnav-right">
+              <a href="php/Login.php">Connexion</a>
+			  <a href="php/Logout.php">Déconnexion</a>
+            </div>
+        </div>
+        <div class="cadre">
+            <h1>
+              Bienvenue sur ce site de Tournois sportifs
+            </h1>
+            <p>
+                - Pas de compte ? Inscrivez-vous dès maintenant -
+            </p>
+        </div>  
+        <div class="inscription">
+            <a href="php/Register.php">INSCRIPTION</a>   
+        </div>
+
     </body>
 </html>
