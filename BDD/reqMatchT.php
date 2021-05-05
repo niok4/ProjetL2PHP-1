90efa614575899ed2c3727bb9ad32980d97695a5
Onur Tan Kulan <46331545+Tnkln99@users.noreply.github.com>
2021-05-05 16:06:03 +0200

    Update reqMatchT.php

--- BDD/reqMatchT.php
+++ BDD/reqMatchT.php
@@ -12,85 +12,120 @@
 		}
 
         $idMt = chooseIntegerIdSequential("MatchT","idMatchT");
-        $requete = "INSERT INTO MatchT VALUES($idMt, $idT,$date, $horaire);";
+
+        $requete = "INSERT INTO MatchT VALUES($idMt,$idT,'$date', '$horaire');";
 
         $res = $connexion->query($requete);
         if(!$res)
 			die('Echec lors de l\'exécution de la requête: ('.$connexion->errno.') '.$connexion->error);
         
-		$connection->close();
+		$connexion->close();
 
         return true;
     }	
+
+	function IsAlreadyProgrammed(int $idT){
+		include('DataBaseLogin.inc.php');
+		
+		$connexion = new mysqli($server, $user, $passwd, $db);
+	
+		if($connexion->connect_error)
+		{
+			echo('Erreur de connexion('.$connexion->connect_errno.') '.$connexion->connect_error);
+		}
+		$requete = "SELECT * FROM MatchT WHERE idMatchT = \"$idT\";";
+
+		$res = $connexion->query($requete);
+		
+		if(!$res)
+			die('Echec lors de l\'exécution de la requête: ('.$connexion->errno.') '.$connexion->error);
+
+		
+		if(mysqli_num_rows($res) >0){
+			return true;
+		}
+		else{
+			return false;
+		}
+
+		$connexion->close();
+	}
+
+	function getMatchT(string $id){
+		{
+			include('DataBaseLogin.inc.php');
+			
+			$connexion = new mysqli($server, $user, $passwd, $db);
+		
+			if($connexion->connect_error)
+			{
+				echo('Erreur de connexion('.$connexion->connect_errno.') '.$connexion->connect_error);
+			}
+			
+			$requete = "SELECT * FROM MatchT WHERE idMatchT = \"$id\";";
+			
+			$res = $connexion->query($requete);
+			if(!$res)
+			{
+				die('Echec lors de l\'exécution de la requête: ('.$connexion->errno.') '.$connexion->error);
+				$connexion->close();
+				
+				return NULL;
+			}
+			
+			$objTemp = $res->fetch_object();
+			$idMatchT = strval($objTemp->idMatchT);
+			$date = strval($objTemp->date);
+			$horaire = strval($objTemp->horaire);
+			$idTournoi = strval($objTemp->idTournoi);
+			
+			$connexion->close();
+			
+			if(empty($idMatchT))
+				return NULL;
+			
+			return new MatchT($idMatchT, $date, $horaire, $idTournoi);
+		}
+	}
+
+	function getAllMatchTwithIdT(string $idT)
+	{
+		include('DataBaseLogin.inc.php');
+		
+		$connexion = new mysqli($server, $user, $passwd, $db);
+	
+		if($connexion->connect_error)
+		{
+			echo('Erreur de connexion('.$connexion->connect_errno.') '.$connexion->connect_error);
+		}
+		
+		$requete = "SELECT *
+		FROM MatchT
+		WHERE idTournoi = $idT;";
+		
+		$res = $connexion->query($requete);
+		if(!$res)
+		{
+			die('Echec lors de l\'exécution de la requête: ('.$connexion->errno.') '.$connexion->error);
+			$connexion->close();
+			
+			return NULL;
+		}
+		
+		$nbMatchT = $res->num_rows;
+		
+		$connexion->close();
+		
+		$tabMatchT = array();
+		
+		if($nbMatchT == 0)
+			return $tabMatchT;
+		
+		while($obj = $res->fetch_object())
+		{
+			array_push($tabMatchT, getMatchT($obj->idMatchT));
+		}
+		
+		return $tabMatchT;
+	}
     
-    function getMatchTwithId(string $id)
-	{
-		include('DataBaseLogin.inc.php');
-		
-		$connexion = new mysqli($server, $user, $passwd, $db);
-	
-		if($connexion->connect_error)
-		{
-			echo('Erreur de connexion('.$connexion->connect_errno.') '.$connexion->connect_error);
-		}
-		
-		$requete = "SELECT * FROM MatchT WHERE idMatchT = \"$id\";";
-		
-		$res = $connexion->query($requete);
-		if(!$res)
-		{
-			die('Echec lors de l\'exécution de la requête: ('.$connexion->errno.') '.$connexion->error);
-			$connexion->close();
-			
-			return NULL;
-		}
-		
-		$objTemp = $res->fetch_object();
-		$idMatcT = strval($objTemp->idMatcT);
-		$idTournoi = strval($objTemp->idTournoi);
-		$date = strval($objTemp->date);
-		$horaire = strval($objTemp->duree);
-		
-		$connexion->close();
-		
-		if(empty($idMatcT))
-			return NULL;
-		
-		return new MatchT($idMatcT, $date, $horaire, $idTournoi);
-	}
-	function getMatchTwithDateAndHoraire(string $date,string $horaire)
-	{
-		include('DataBaseLogin.inc.php');
-		
-		$connexion = new mysqli($server, $user, $passwd, $db);
-	
-		if($connexion->connect_error)
-		{
-			echo('Erreur de connexion('.$connexion->connect_errno.') '.$connexion->connect_error);
-		}
-		
-		$requete = "SELECT * FROM MatchT WHERE date = \"$date\" AND horaire = \"$horaire\";";
-		
-		$res = $connexion->query($requete);
-		if(!$res)
-		{
-			die('Echec lors de l\'exécution de la requête: ('.$connexion->errno.') '.$connexion->error);
-			$connexion->close();
-			
-			return NULL;
-		}
-		
-		$objTemp = $res->fetch_object();
-		$idMatcT = strval($objTemp->idMatcT);
-		$idTournoi = strval($objTemp->idTournoi);
-		$date = strval($objTemp->date);
-		$horaire = strval($objTemp->duree);
-		
-		$connexion->close();
-		
-		if(empty($idMatcT))
-			return NULL;
-		
-		return new MatchT($idMatcT, $date, $horaire, $idTournoi);
-	}
-?>
