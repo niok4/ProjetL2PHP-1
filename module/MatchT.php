7e434a18c41251cafa4bc9487d0f2fc0df0f5a48
Onur Tan Kulan <46331545+Tnkln99@users.noreply.github.com>
2021-05-05 16:06:31 +0200

    Update MatchT.php

--- module/MatchT.php
+++ module/MatchT.php
@@ -1,28 +1,72 @@
 <?php
-
-class MatchT {
-	protected $m_idMatchT ;
+	include_once('Entite.php');
 	
-	//Constructeur
-	public function __construct(int $id)
+	class MatchT extends Entite
+	{
+		protected $m_idMatchT;
+		protected $m_date;
+		protected $m_horaire;
+		protected $m_idTournois;
+		
+		//Constructeur
+		public function __construct(int $id, string $date, string $horaire,int $idT)
 		{
-			$this->m_idMatchT = $id ;
+			$this->m_idMatchT = $id;
+			$this->m_date = $date;
+			$this->m_horaire = $horaire;
+			$this->m_idTournois = $idT;
+		}
+		
+		//ACESSEURS EN LECTURE
+		public function afficher()
+		{
+			echo "Match n°".$this->m_idMatchT;
+			echo "<br ./>";
+		}
+		
+		public function getIdMatchT()
+		{
+			return $this->m_idMatchT;
+		}
+		
+		public function getdateMatchT()
+		{
+			return $this->m_date;
 		}
 
-	//ACESSEURS EN LECTURE
-	public function afficher(){
-		echo "Match n°".$this->m_idMatchT;
-		echo "<br ./>";
-	}
+		public function gethoraire()
+		{
+			return $this->m_horaire;
+		}
 
-	public function getIdMatchT(){
-		return $this->m_idMatchT;
-	}
+		public function getIdTournoi()
+		{
+			return $this->m_idTournois;
+		}
+		//ACCESSEURS EN ECRITURE
+		public function setId(int $id)
+		{
+			$this->m_idMatchT = $id;
+		}
 
-	//ACCESSEURS EN ECRITURE
-	public function setId(int $id){
-		$this->m_idMatchT = $id ;
-	}
+		public function setDate(string $date)
+		{
+			$this->$m_date = $date;
+		}
 
-}
+		public function setHoraire(string $horaire)
+		{
+			$this->m_horaire = $horaire;
+		}
+		
+		public function toString()
+		{
+			return strval($this->m_idMatchT);
+		}
+		
+		public function toHTML()
+		{
+			return "<p>".strval($this->m_idMatchT)."</p>";
+		}
+	}
 ?>
